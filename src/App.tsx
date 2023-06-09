6a0ba8b928f1aad85b0ccd33c9e04bbfbb1c244b
Aldo Espinosa <aldo@altomobile.io>
2023-06-09 13:21:19 -0600

    RTK Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { ProductList } from "./pages";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <ProductList />
+    </Provider>
+  );
 };
