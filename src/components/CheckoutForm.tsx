f5085d35d2f292cf74461aaa23661621b64e73a6
Aldo Espinosa <aldo@altomobile.io>
2023-06-09 16:08:02 -0600

    Form Working

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -1,13 +1,35 @@
+import { ChangeEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import { luhnCheck } from "../helpers";
+import { useAppSelector } from "../hooks";
+import { RootState } from "../store";
 
 export const CheckoutForm = () => {
+  const { items } = useAppSelector((state: RootState) => state.basket);
+
+  const [cardNumber, setCardNumber] = useState<string>("");
+  const onSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(luhnCheck(cardNumber));
+    console.log({
+      basket: items.map((item) => ({ sku: item.sku, quantity: item.quantity })),
+      cardNumber,
+    });
+  };
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCardNumber(e.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="flex flex-row justify-center items-center">
         <label>Input Your Card Number</label>
         <input
+          value={cardNumber}
           className="w-1/4 p-2 border rounded border-gray-400 ml-4"
           type="text"
+          onChange={onInputChange}
         />
       </div>
       <div className="flex flex-row justify-end mt-6 px-8">
@@ -15,6 +37,7 @@ export const CheckoutForm = () => {
           Continue Shopping
         </Link>
         <button
+          disabled={items.length <= 0}
           type="submit"
           className="ml-4 text-s p-2 rounded border border-blue-600 bg-blue-500 hover:bg-blue-700 text-white"
         >
